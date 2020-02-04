c19a73e3fe03ed79806ab687e3d4b30028056e13
EC2 Default User <ec2-user@ip-172-31-47-176.ap-northeast-1.compute.internal>
2020-02-04 04:23:08 +0000

    グループモデル作成

--- db/migrate/20200116155138_create_users.rb
+++ db/migrate/20200116155138_create_users.rb
@@ -12,3 +12,13 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     end
   end
 end
+
+class CreateGroups < ActiveRecord::Migration[5.2]
+  def change
+    create_table :groups do |t|
+      t.string :name, unique: true
+      t.timestamps
+    end
+  end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_16_155138) do
+ActiveRecord::Schema.define(version: 2020_02_04_030852) do
+
+  create_table "groups", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_01_16_155138) do
     t.text "like_weapon"
     t.string "image"
     t.string "remember_digest"
+    t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
