2d2980dc0005ea0a5716f5f584ce5265b8e9520d
EC2 Default User <ec2-user@ip-172-31-47-176.ap-northeast-1.compute.internal>
2020-02-05 04:49:12 +0000

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root   'top_page#home'
-  root 'groups#index'
   get   '/signup',   to: 'users#new'
   post  '/signup',   to: 'users#create'
   
@@ -9,5 +8,5 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   resource   :users,  only: [:edit, :update, :destroy]
   resources  :users,  only: [:show, :index]
-  resources  :groups, only: [:new, :create, :edit, :update]
+  resources  :groups
 end
