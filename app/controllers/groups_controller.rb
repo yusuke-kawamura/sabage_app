5c055e7d8a604ae6a8e79280d49f77826b384fae
EC2 Default User <ec2-user@ip-172-31-47-176.ap-northeast-1.compute.internal>
2020-02-09 04:12:24 +0000

    controller追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,6 +21,19 @@ class GroupsController < ApplicationController
     end
   end
   
+  def edit
+    @group = Group.find(params[:id])
+  end
+  
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to group_path, success: "プロフィールを更新しました"
+    else
+      render 'edit'
+    end
+  end
+  
   private
   
   def group_params
