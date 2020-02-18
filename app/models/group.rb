603edec76fae4111055c197c61642f211435b54f
EC2 Default User <ec2-user@ip-172-31-47-176.ap-northeast-1.compute.internal>
2020-02-18 06:58:11 +0000

    関連付け

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
+  belongs_to :user, foreign_key: 'group_creator_id'
   validates :name, presence: true, uniqueness: true
 end
